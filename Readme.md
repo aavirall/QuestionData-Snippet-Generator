3dc5e15023255bc551b1f18ea07f66103d66fdad
Aviral <aviral.rana@juleo.club>
2023-12-15 01:59:29 +0530

    Added contents to README

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# QuestionData Snippet Generator
+
+- Just run `app.py`, snippet will be generated in `out.txt` file.
+- Paste the contents of `out.txt` file into the `Message` body to ConsumerAPI/ProcessPlans gRPC call.
+- Done!
--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import re,q1,q2,q3,q4,q5,q6,q7,q8,q15,q16
 
 from Question import *
 
-arr = [q1.rep, q2.rep, q3.rep, q4.rep, q5.rep, q6.rep, q7.rep, q8.rep, q15.rep, q16.rep]
+questions = [q1.rep, q2.rep, q3.rep, q4.rep, q5.rep, q6.rep, q7.rep, q8.rep, q15.rep, q16.rep]
 
 
 #-----------------------------------------------------------------------------------#
@@ -15,9 +15,9 @@ templ = """
 """
 if __name__ == "__main__":
     i = 1
-    for ele in arr:
+    for ele in questions:
         templ = re.sub("INDEX", str(i), templ)
-        if i != len(arr)-1:
+        if i != len(questions)-1:
             templ = re.sub("QUESTION", str(ele)+",\n\t\t"+'"INDEX":QUESTION', templ)
         else:
             templ = re.sub("QUESTION", str(ele), templ)
