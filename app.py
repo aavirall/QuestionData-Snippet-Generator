8a5b486f170585e9c585434f5311e5f44a0ee737
Aviral <aviral.rana@juleo.club>
2023-12-15 02:03:57 +0530

    Refactored file structure

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-import re,q1,q2,q3,q4,q5,q6,q7,q8,q15,q16
-
-from Question import *
+import re
+from Questions import q1,q2,q3,q4,q5,q6,q7,q8,q15,q16
 
 questions = [q1.rep, q2.rep, q3.rep, q4.rep, q5.rep, q6.rep, q7.rep, q8.rep, q15.rep, q16.rep]
 
